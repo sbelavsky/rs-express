078dd7b0ea372b00c303e9934a11393749cf0489
Sergey Belavsky <sergey.belavsky@idt.net>
2020-10-09 18:34:29 +0300

    fix: change method name

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -8,18 +8,18 @@ const create = async user => {
   DB.users.push(user);
 };
 
-const getByID = async id => {
+const get = async id => {
   return DB.users.find(u => u.id === id);
 };
 
 const update = async user => {
   const index = DB.users.findIndex(u => u.id === user.id);
   DB.users[index] = user;
-  return await getByID(user.id);
+  return await get(user.id);
 };
 
 const remove = async id => {
   DB.users = DB.users.filter(u => u.id === id);
 };
 
-module.exports = { getAll, create, getByID, update, remove };
+module.exports = { getAll, create, getByID: get, update, remove };
